640eeed2f27aef35ac0ff90e40865123876b8242
Sitkus <luk@box4metal.com>
2021-06-08 09:15:30 +0300

    Slightly refactored code style, added sass-loader, seperated scss

--- src/components/GeneratedBill.vue
+++ src/components/GeneratedBill.vue
@@ -1,32 +1,33 @@
 <template>
-
   <div id="bill" style="font-family: Arial,serif; padding: 20px;">
-
-    <h3>{{bill.name[0]}}</h3>
-    <h3>{{bill.id[0]}}</h3>
-    <div>{{bill.date[0]}}</div>
+    <h3>{{ bill.name[0] }}</h3>
+    <h3>{{ bill.id[0] }}</h3>
+    <div>{{ bill.date[0] }}</div>
 
     <div style="display: flex; margin-top: 20px">
       <div id="sellerInfo" style="width: 50%">
-        <h4>{{bill.participants.seller[0]}}</h4>
+        <h4>{{ bill.participants.seller[0] }}</h4>
         <div v-for="(item, index) in bill.seller" :key="index">
-          {{item[0]}}
+          {{ item[0] }}
         </div>
       </div>
 
       <div id="buyerInfo" style="width: 50%">
         <h4>{{ bill.participants.buyer[0] }}</h4>
         <div v-for="(item, index) in bill.buyer" :key="index">
-          {{item[0]}}
+          {{ item[0] }}
         </div>
       </div>
-
     </div>
 
     <div style="margin-top: 50px">
       <table style="width:100%; border-collapse: collapse;">
         <tr>
-          <th style="text-align: start; padding: 5px; border: 1px solid black" v-for="(item, index) in bill.table.header" :key="index">
+          <th
+            style="text-align: start; padding: 5px; border: 1px solid black"
+            v-for="(item, index) in bill.table.header"
+            :key="index"
+          >
             {{ item[0] }}
           </th>
         </tr>
@@ -44,55 +45,48 @@
 
     <div v-if="bill.showNotes" style="margin-top: 100px; max-width: 770px">
       <div style="margin-bottom: 20px">
-        {{bill.notes.title[0]}}
+        {{ bill.notes.title[0] }}
       </div>
       <div>
-        {{bill.notes.value[0]}}
+        {{ bill.notes.value[0] }}
       </div>
     </div>
 
-    <div style="display: flex; justify-content: space-between; margin-top: 100px"
-    v-if="bill.showSignatures">
-
+    <div style="display: flex; justify-content: space-between; margin-top: 100px" v-if="bill.showSignatures">
       <div style="width: 200px">
         {{ bill.props.seller[0] }}
-        <div style="padding: 30px 0px" :style="signatureSpace(true)">{{bill.signatures.seller[0]}}</div>
+        <div style="padding: 30px 0px" :style="signatureSpace(true)">{{ bill.signatures.seller[0] }}</div>
         <div style="border-bottom: 1px solid black; margin-bottom: 5px"></div>
-        <div><small>{{ bill.props.sellerSign[0] }}</small></div>
+        <div>
+          <small>{{ bill.props.sellerSign[0] }}</small>
+        </div>
       </div>
 
       <div style="width: 200px">
         {{ bill.props.buyer[0] }}
-        <div style="padding: 30px 0px" :style="signatureSpace(false)"
-        >{{bill.signatures.buyer[0]}}</div>
+        <div style="padding: 30px 0px" :style="signatureSpace(false)">{{ bill.signatures.buyer[0] }}</div>
         <div style="border-bottom: 1px solid black; margin-bottom: 5px"></div>
-        <div><small>{{ bill.props.buyerSign[0] }}</small></div>
+        <div>
+          <small>{{ bill.props.buyerSign[0] }}</small>
+        </div>
       </div>
-
     </div>
-
-
-
   </div>
 </template>
 
 <script>
 export default {
-name: "generatedBill",
+  name: 'GeneratedBill',
   props: ['bill'],
   methods: {
     signatureSpace(seller) {
-      if(seller && this.bill.signatures.seller[0].length === 0) {
-        return {height: 18+ "px"}
+      if (seller && this.bill.signatures.seller[0].length === 0) {
+        return { height: 18 + 'px' };
       }
-      if(!seller && this.bill.signatures.buyer[0].length === 0) {
-        return {height: 18+ "px"}
+      if (!seller && this.bill.signatures.buyer[0].length === 0) {
+        return { height: 18 + 'px' };
       }
     }
   }
-}
+};
 </script>
-
-<style scoped>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
-import { createApp } from 'vue'
-import App from './App.vue'
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-
-createApp(App).mount('#app')
-
+createApp(App).mount('#app');
